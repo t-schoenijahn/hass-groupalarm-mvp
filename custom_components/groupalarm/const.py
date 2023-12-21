bb26a523ae934dfced0cb25843a6c9aee77dfe9f
t.schoenijahn <tom.schoenijahn@rwth-aachen.de>
2023-12-21 15:38:42 +0100

    switch paths to GroupAlarm

--- custom_components/groupalarm/const.py
+++ custom_components/groupalarm/const.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 DOMAIN = "groupalarm"
 
-DEFAULT_NAME = "GroupAlarm 24/7"
+DEFAULT_NAME = "GroupAlarm"
 DEFAULT_SHORT_NAME = "GroupAlarm"
 
 ATTR_NAME = "state"
@@ -13,7 +13,7 @@ GROUPALARM_COORDINATOR = "groupalarm_coordinator"
 GROUPALARM_NAME = "groupalarm_name"
 
 DEFAULT_TIMEOUT = 10
-GROUPALARM_URL = "https://www.groupalarm247.com/api/v2/pull/all"
-GROUPALARM_STATUS_URL = "https://www.groupalarm247.com/api/v2/statusgeber/set-status"
+GROUPALARM_URL = "https://app.groupalarm.com/api/v1"
+
 
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=1)
